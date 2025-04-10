c6f9c2d66a837bf8b75aa71b44a328e2261db798
lhuang20 <lukehmtn@gmail.com>
2025-04-09 20:27:03 -0700

    Revert "Transfered R functions from main repo to package repo"
    This reverts commit 16f840fa04757ce83cd380a55f98bd75136bef01.

--- R/roc_plot.R
+++ R/roc_plot.R
@@ -9,30 +9,31 @@
 #' @param output_path A character string specifying the file path to save the ROC plot.
 #'
 #' @return A ggplot2 object of the ROC curve.
-#'
+#' @importFrom grDevices pdf
 #' @export
-#' 
 #' @examples
 #' # Example usage:
 #' # Assuming you have model outputs, true labels, predicted probabilities, and AUC value
-#' roc_plot(lasso_model_outputs, "Diabetes_binary", ".pred_1", lasso_metrics$.estimate[lasso_metrics$.metric == "roc_auc"], "roc_curve_plot.png")
-#' 
+#' \dontrun{
+#'   roc_plot(lasso_model_outputs, "Diabetes_binary", ".pred_1", 
+#'            lasso_metrics$.estimate[lasso_metrics$.metric == "roc_auc"], "roc_curve_plot.png")
+#' }
+utils::globalVariables(c("specificity", "sensitivity"))
 roc_plot <- function(model_outputs, true_class, predicted_probs, roc_auc_value, output_path) {
-
   # Check for missing or empty inputs
   if (nrow(model_outputs) == 0 || is.null(model_outputs) || ncol(model_outputs) == 0) {
     stop("Error: Model outputs or columns cannot be empty.")
   }
-
+  
   # Check if the columns exist in model_outputs
   if (!true_class %in% colnames(model_outputs)) {
     stop(paste("Error: Column '", true_class, "' not found in model outputs.", sep = ""))
   }
-
+  
   if (!predicted_probs %in% colnames(model_outputs)) {
     stop(paste("Error: Column '", predicted_probs, "' not found in model outputs.", sep = ""))
   }
-
+  
   # Validate AUC value
   if (is.na(roc_auc_value)) {
     stop("Error: AUC value cannot be missing.")
@@ -40,15 +41,15 @@ roc_plot <- function(model_outputs, true_class, predicted_probs, roc_auc_value, 
   if (roc_auc_value < 0 || roc_auc_value > 1) {
     stop("Error: AUC value must be between 0 and 1.")
   }
-
+  
   # Validate output path
   if (output_path == "") {
     stop("Error: Output file path cannot be empty.")
   }
-
+  
   # solve interactive issue
   if(!interactive()) pdf(NULL)
-
+  
   # Create the ROC plot
   roc_curve_data <- yardstick::roc_curve(
     model_outputs, 
@@ -56,7 +57,7 @@ roc_plot <- function(model_outputs, true_class, predicted_probs, roc_auc_value, 
     !!rlang::sym(predicted_probs), 
     event_level = "second"
   )
-
+  
   output_plot <- ggplot2::ggplot(roc_curve_data, 
                                  ggplot2::aes(x = 1 - specificity, y = sensitivity)) +
     ggplot2::geom_path() +
@@ -74,10 +75,10 @@ roc_plot <- function(model_outputs, true_class, predicted_probs, roc_auc_value, 
       title = "ROC Curve"
     ) +
     ggplot2::theme_minimal()
-
+  
   # Save the ROC plot to the specified file path
-  ggplot2::ggsave(output_path, output_plot)
-
+  ggplot2::ggsave(output_path, output_plot, width = 8, height = 8, dpi = 300, limitsize = FALSE)
+  
   # Return the ROC plot object
   return(output_plot)
 }
