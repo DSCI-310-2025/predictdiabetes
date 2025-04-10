16f840fa04757ce83cd380a55f98bd75136bef01
lhuang20 <lukehmtn@gmail.com>
2025-04-09 20:23:50 -0700

    Transfered R functions from main repo to package repo

--- R/plots_grid.R
+++ R/plots_grid.R
@@ -1,11 +1,10 @@
 #' Grid of Plots
 #'
-#' Combine a list or mulitple lists of \code{ggplot2} objects into a single
+#' Facet a list of \code{ggplot2} bar plot objects into a single
 #' grid layout using \code{patchwork}. The number of columns in the grid can be
 #' specified with `num_cols`.
 #'
-#' @param bar_plots A list of bar plot \code{ggplot2} objects.
-#' @param density_plots A list of quantitative density plot \code{ggplot2} objects.
+#' @param bar_plots A list of \code{ggplot2} bar plot objects.
 #' @param num_cols The number of columns in the grid (Default = 3).
 #'
 #' @return A \code{patchwork} / \code{ggplot2} object showing all input plots arranged.
@@ -19,26 +18,19 @@
 #'     data_frame = mtcars,
 #'     cat_vars = c("cyl", "gear"),
 #'     target_col = "am"
-#'   )
-#'   # Generate example density plots
-#'   density_plots <- quantitative_density(
-#'     data_frame = mtcars,
-#'     noncat_vars = c("mpg", "hp", "wt"),
-#'     target_col = "am"
-#'   )
+#'     )
+#'     
 #'   # Combine plots into a grid
-#'   grid_plot <- plots_grid(bar_plots, density_plots, num_cols = 2)
+#'   grid_plot <- plots_grid(bar_plots, num_cols = 2)
 #' }
 #'
-plots_grid <- function(bar_plots, density_plots, num_cols = 3) {
+plots_grid <- function(bar_plots, num_cols = 3) {
   
-  if (!is.list(bar_plots) || !all(sapply(bar_plots, inherits, "gg")) || 
-      !is.list(density_plots) || !all(sapply(density_plots, inherits, "gg"))) {
-    stop("Both 'bar_plots' and 'density_plots' must be lists of ggplot objects.")
+  if (!is.list(bar_plots) || !all(sapply(bar_plots, inherits, "gg"))) {
+    stop("'bar_plots' must be a list of ggplot objects.")
   }
   
-  all_plots <- c(bar_plots, density_plots)
-  combined_plots <- patchwork::wrap_plots(all_plots, ncol = num_cols) +
+  combined_plots <- patchwork::wrap_plots(bar_plots, ncol = num_cols) +
     patchwork::plot_layout(guides = "collect") +
     patchwork::plot_annotation(
       theme = ggplot2::theme(
@@ -48,5 +40,6 @@ plots_grid <- function(bar_plots, density_plots, num_cols = 3) {
         axis.text = ggplot2::element_text(size = 30)
       )
     )
+  
   return(combined_plots)
 }
